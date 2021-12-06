7a3a9096d7778e8065056818621fd759fd9be961
erikalee <slperika@hotmail.com>
2021-12-05 19:53:46 -0600

    changes

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React from "react";
 
 /**
  * We need to import fetchQueryResultsFromURL since we will sometimes have urls in info.prev and info.next
  * which are query urls.
  */
-import { fetchQueryResultsFromURL } from '../api';
+import { fetchQueryResultsFromURL } from "../api";
 
 const Preview = (props) => {
   /**
    * Destructure setSearchResults, setFeaturedResult, and setIsLoading from props
    * and also destructure info and records from props.searchResults
-   * 
+   *
    * You need info, records, setSearchResults, setFeaturedResult, and setIsLoading as available constants
    */
 
-
   /**
    * Don't touch this function, it's good to go.
-   * 
+   *
    * It has to be defined inside the Preview component to have access to setIsLoading, setSearchResults, etc...
    */
   async function fetchPage(pageUrl) {
@@ -36,8 +35,8 @@ const Preview = (props) => {
   return <aside id="preview">
     <header className="pagination">
       {/* This button should be disabled if nothing is set in info.prev, and should call fetchPage with info.prev when clicked */}
-      <button 
-        disabled={} 
+      <button
+        disabled={}
         className="previous"
         onClick={}>Previous</button>
       {/* This button should be disabled if nothing is set in info.next, and should call fetchPage with info.next when clicked */}
@@ -49,15 +48,15 @@ const Preview = (props) => {
     <section className="results">
       {
         /* Here we should map over the records, and render something like this for each one:
-          <div  
+          <div
             key={ index }
             className="object-preview"
             onClick={(event) => {
               // prevent the default
               // set the featured result to be this record, using setFeaturedResult
             }}>
-            { 
-              // if the record.primaryimageurl exists, show this: <img src={ record.primaryimageurl } alt={ record.description } />, otherwise show nothing 
+            {
+              // if the record.primaryimageurl exists, show this: <img src={ record.primaryimageurl } alt={ record.description } />, otherwise show nothing
             }
             {
               // if the record.title exists, add this: <h3>{ record.title }</h3>, otherwise show this: <h3>MISSING INFO</h3>
@@ -67,6 +66,6 @@ const Preview = (props) => {
       }
     </section>
   </aside>
-}
+};
 
 export default Preview;
